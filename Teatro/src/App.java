e9218640022a3677ef55949c6b73bbdcb2b0f193
AmeMeida <ame.pistache@gmail.com>
2022-06-03 09:09:59 -0300

    N sei

--- Teatro/src/App.java
+++ Teatro/src/App.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
+import Enums.Tema;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Tema novoTema = Tema.COMEDIA;
+        novoTema = Tema.ACAO;
+
+        novoTema.setTema("ação");
     }
 }
--- Teatro/src/Controller.java
+++ Teatro/src/Controller.java
@@ -2,5 +2,5 @@ import java.util.ArrayList;
 import Classes.Cliente;
 
 public final class Controller {
-    private ArrayList<Cliente> lstCliente = ArrayList<Cliente>;
+    private ArrayList<Cliente> lstCliente = new ArrayList<Cliente>();
 }
